301fb77638348f846c4aacb44cb1d77fc9a16cba
merkost <konstantinlikes@gmail.com>
2021-10-16 21:24:07 +1000

    refactoring

--- app/src/main/java/com/edu/github_mvp/mvp/model/entity/GithubUser.kt
+++ app/src/main/java/com/edu/github_mvp/mvp/model/entity/GithubUser.kt
@@ -1,6 +1,7 @@
 package ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.entity
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
 import com.google.gson.annotations.Expose
 import kotlinx.android.parcel.Parcelize
 
